766e8c9aff6d90be75879f651440e06dffdd8fab
Fritscher <fritscher.dev@gmail.com>
2024-01-19 18:26:19 +0100

    fix: remove middleware for testing

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,32 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cors from 'cors';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
 
 // Middlewares
-import { protectedRoute } from './middlewares/protectedMiddleware';
-import { errorHandler, notFound } from './middlewares/errorMiddlewares';
+import { protectedRoute } from "./middlewares/protectedMiddleware";
+import { errorHandler, notFound } from "./middlewares/errorMiddlewares";
 
 // Routes
-import api from './routes';
+import api from "./routes";
 
 // Utils
-require('dotenv').config();
+require("dotenv").config();
 
 // App
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 // Routes
 // Before API Route
-app.use(protectedRoute);
+// app.use(protectedRoute);
 
 // API Routes
-app.use('/api/v1', api);
+app.use("/api/v1", api);
 
 // After API Route
 app.use(notFound);
