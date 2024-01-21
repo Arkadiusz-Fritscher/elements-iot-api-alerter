38712b3bb966369b4a37a6bef6f75fe5d171d0a8
Fritscher <fritscher.dev@gmail.com>
2024-01-21 20:34:39 +0100

    feat: change port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import app from "./app";
 
 // Start the server
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
